7ba024f348471f2fb17600aac28b7c85c1a50cc3
Daniel Ingber <diiamfbasfd@gmail.com>
2026-07-02 02:16:28 +0300

    initial commit

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import flowbiteReact from "flowbite-react/plugin/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/booklibrary/',
+  plugins: [react(), tailwindcss(), flowbiteReact()],
+});
